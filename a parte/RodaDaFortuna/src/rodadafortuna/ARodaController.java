9e23c27e61e30c7768d5da2270e9eb28b481e377
Higor-Tz <62813622+Higor-Tz@users.noreply.github.com>
2020-07-21 00:57:20 -0300

    alterações extras

--- a parte/RodaDaFortuna/src/rodadafortuna/ARodaController.java
+++ a parte/RodaDaFortuna/src/rodadafortuna/ARodaController.java
@@ -7,7 +7,16 @@ package rodadafortuna;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
+import javafx.scene.effect.SepiaTone;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 /**
  * FXML Controller class
@@ -16,12 +25,162 @@ import javafx.fxml.Initializable;
  */
 public class ARodaController implements Initializable {
 
+   @FXML
+    private GridPane paneJogadores;
+
+    @FXML
+    private Button comprarVogal, girarRoda, resolverPuzzle;
+
+    @FXML
+    private ImageView imagemRoda;
+
+    @FXML
+    private Label labelPuzzle;
+
+    @FXML
+    private GridPane paneVogais, paneConsoantes;
+
+    private final int quantidadeJogadores = 3;
+    private final int custoVogal = 250;
+    private int posicaoJogadorAtual = 0;
+
+    private Tabuleiro tabuleiro;
+    private Roda roda;
+    private final Jogador[] jogadores = new Jogador[quantidadeJogadores];
+    private Jogador jogadorAtual;
+
+    private final SepiaTone selected = new SepiaTone(1.5);
+
     /**
-     * Initializes the controller class.
+     * Inicializa a cena da primeira jogada.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    @FXML
+    private void initialize() {
+        // bloquear comprarVogal
+        this.comprarVogal.disableProperty().set(true);
+
+        // instanciar a Roda
+        this.roda = new Roda();
+        // setar a imagem atual da Roda
+        //imagemRoda.setImage(this.roda.getImagemAtual()); // remover comentario, 
+                                                           // apos implementar a classe Roda
+
+        // bloquear o painel das vogais
+        this.paneVogais.disableProperty().set(true);
+        // setar o evento clicarVogal em cada botao das vogais
+        this.paneVogais.getChildren().forEach(n -> ((Button) n).setOnAction(e -> clicarVogal(e)));
+
+        // bloquear o painel das consoantes
+        this.paneConsoantes.disableProperty().set(true);
+        // setar o evento clicarConsoante em cada botao das consoantes
+        this.paneConsoantes.getChildren().forEach(n -> ((Button) n).setOnAction(e -> clicarConsoante(e)));
+        
+    }
+
+    /**
+     * Implementa a logica de comprar uma vogal.
+     * @param event 
+     */
+    @FXML
+    private void comprarVogalAction(ActionEvent event) {
+        
+    }
+
+    /**
+     * Implementa a logica de girar a roda.
+     * @param event 
+     */
+    @FXML
+    private void girarRodaAction(ActionEvent event) {
+        
+    }
+
+    /**
+     * Implementa a logica de tentar adivinhar o puzzle.
+     * @param event 
+     */
+    @FXML
+    private void resolverPuzzleAction(ActionEvent event) {
+        
+    }
+
+    /**
+     * Implementa a logica de clicar em uma CONSOANTE.
+     * @param event 
+     */
+    private void clicarConsoante(ActionEvent event) {
+        
+    }
+
+    /**
+     * Implementa a logica de clicar em uma VOGAL.
+     * @param event 
+     */
+    private void clicarVogal(ActionEvent event) {
+        
+    }
+
+    /**
+     * Verifica se o jogo terminou, ou seja, se todas as letras do puzzle foram preenchidas,
+     * e, em caso positivo, exibe uma mensagem informando quem ganhou e termina a aplicacao.
+     */
+    private void isFimDoJogo() {
+        
+    }
+
+    /**
+     * Valida se todos os botoes das VOGAIS foram clicados, i.e., ficaram desativados.
+     * @return boolean
+     */
+    private boolean isVogaisEsgotadas() {
+        return false; // remover
+    }
+
+    /**
+     * Valida se todos os botoes das CONSOANTES foram clicados, i.e., ficaram desativados.
+     * @return boolean
+     */
+    private boolean isConsoantesEsgotadas() {
+        return false; // remover
+    }
+
+    /**
+     * Cria e exibe uma janela de alerta.
+     * @param type
+     * @param titulo
+     * @param conteudo 
+     */
+    private void alerta(Alert.AlertType type, String titulo, String conteudo) {
+
+    }
+
+    /**
+     * Avanca para o proximo jogador.
+     * Adiciona o efeito de selecionado, i.e., <code>selected</code>, 
+     * no <code>TitledPane</code> do jogador atual.
+     */
+    private void avancarProximoJogador() {
+        
+    }
+
+    /**
+     * Recebe o puzzle e os nomes dos jogadores.
+     * @param puzzle
+     * @param nomeJogadores 
+     */
+    public void setPuzzleENomeJogadores(final String puzzle, final String... nomeJogadores) {
+        this.tabuleiro = new Tabuleiro(puzzle);
+        this.labelPuzzle.setText(this.tabuleiro.getPalpitePuzzle());
+
+        for (int i = 0; i < quantidadeJogadores; i++) {
+            ((TitledPane) this.paneJogadores.getChildren().get(i)).setText(nomeJogadores[i]);
+
+            this.jogadores[i] = new Jogador(nomeJogadores[i]);
+            this.jogadores[i].setRoda(this.roda);
+        }
+
+        this.jogadorAtual = this.jogadores[posicaoJogadorAtual];
+        ((TitledPane) this.paneJogadores.getChildren().get(posicaoJogadorAtual)).setEffect(selected);
+    }
+
 }
--- a parte/RodaDaFortuna/src/rodadafortuna/BaseDaRodaController.java
+++ a parte/RodaDaFortuna/src/rodadafortuna/BaseDaRodaController.java
@@ -5,31 +5,52 @@
  */
 package rodadafortuna;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
  * @author higor
  */
 public class BaseDaRodaController implements Initializable {
-    
+     
     @FXML
-    private Label label;
-    
+    private Button iniciar;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField nomeJogador1, nomeJogador2, nomeJogador3, puzzle;
+
+    /**
+     * Mostra o tabuleiro do jogo apos enviar os nomes dos jogadores e o puzzle inseridos.
+     * @param event
+     * @throws IOException 
+     */
+    @FXML
+    private void mostrarTabuleiroAction(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("aRoda.fxml"));
+        Parent root = loader.load();
+
+        ARodaController tabuleiroController = loader.getController();
+        tabuleiroController.setPuzzleENomeJogadores(puzzle.getText(), nomeJogador1.getText(), nomeJogador2.getText(), nomeJogador3.getText());
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Roda da Fortuna");
+        stage.resizableProperty().set(false);
+        stage.show();
+        
+        ((Stage) iniciar.getScene().getWindow()).close(); // fechar Introducao
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
 }
